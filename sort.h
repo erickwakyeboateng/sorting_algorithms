fc1c07c7560979d2bc552e7d553c4218eb38726e
eric <boateng.eric44@gmail.com>
2023-07-25 14:17:46 +0000

    radix

--- sort.h
+++ sort.h
@@ -34,4 +34,5 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 listint_t *swap_node(listint_t *node, listint_t **list);
 void counting_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 #endif
